2b4db1bc9066bfad4a65b18226dfb8e46e0c68d5
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-11-16 17:09:53 +0800

    update: change soure map type

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: 'source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
