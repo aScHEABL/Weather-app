f44d772215112490e0ef7fcec081ce3242e83a73
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-11-16 15:22:15 +0800

    update: format codebase

--- src/geocodingAPI.js
+++ src/geocodingAPI.js
@@ -1,11 +1,17 @@
-export async function geoLocationConvert() {} {
-    const cityName = "hong kong";
-    const limitNumber = "1";
-    const apiKey = "9b708ac24f65eeeba73e728c5a9e1d80";
-    const apiCall = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=" + limitNumber
-    + "&appid=" + apiKey;
+export async function geoLocationConvert() {}
+{
+  const cityName = "hong kong";
+  const limitNumber = "1";
+  const apiKey = "9b708ac24f65eeeba73e728c5a9e1d80";
+  const apiCall =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityName +
+    "&limit=" +
+    limitNumber +
+    "&appid=" +
+    apiKey;
 
-    const response = await fetch(apiCall, {mode: "cors"});
-    const geoData = await response.json();
-    console.log(apiCall);
+  const response = await fetch(apiCall, { mode: "cors" });
+  const geoData = await response.json();
+  console.log(apiCall);
 }
