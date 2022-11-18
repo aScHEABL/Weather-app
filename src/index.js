b35d63d2b706ea61b69e850ea3748ccedd48b615
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-11-18 14:23:31 +0800

    update: fix some typo in comments

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import weatherDataAPI from "./weatherDataAPI";
 
 const searchInput_DOM = document.querySelector("[data-search-input]");
 const searchButton_DOM = document.querySelector("[data-search-button]");
-let cityName = "";
 
 searchButton_DOM.addEventListener("click", getWeatherData);
 
 async function getWeatherData() {
     console.log("Search button has been clicked.");
-    cityName = searchInput_DOM.value;
-    //check if the search input is empty, if not then convert space to +
+    let cityName = searchInput_DOM.value;
+    // Check if the search input is empty, if not then convert space to +
     cityName = stringChecker(cityName);
-    // if boolean returns true, search for the city, if false exit the function
+    // If boolean returns true, search for the city, if false exit the function
     if (cityName) {
         const coordinates = await geoLocationConverterAPI(cityName);
         const weatherData = await weatherDataAPI(coordinates);
